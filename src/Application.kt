03d5e0c01ccd489a990b1321bc086713a7b9afc0
shinobi9 <shinobi9c@outlook.com>
2021-07-08 15:52:10 +0800

    init

---
+++ src/Application.kt
@@ -0,0 +1,57 @@
+package cyou.shinobi9
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.engine.cio.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
+import java.time.*
+import io.ktor.client.features.websocket.*
+import io.ktor.client.features.websocket.WebSockets
+import io.ktor.http.cio.websocket.Frame
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import io.ktor.client.features.logging.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    val client = HttpClient(CIO) {
+        install(HttpTimeout) {
+        }
+        install(Logging) {
+            level = LogLevel.HEADERS
+        }
+    }
+
+    install(io.ktor.websocket.WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        webSocket("/myws/echo") {
+            send(Frame.Text("Hi from server"))
+            while (true) {
+                val frame = incoming.receive()
+                if (frame is Frame.Text) {
+                    send(Frame.Text("Client said: " + frame.readText()))
+                }
+            }
+        }
+    }
+}
+
