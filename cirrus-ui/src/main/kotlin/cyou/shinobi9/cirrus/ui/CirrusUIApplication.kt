60d3f6ae942d18dddeb4784d32ae62a6dcc7a531
shinobi9 <shinobi9c@outlook.com>
2021-07-24 15:12:29 +0800

    close jvm when close window

--- cirrus-ui/src/main/kotlin/cyou/shinobi9/cirrus/ui/CirrusUIApplication.kt
+++ cirrus-ui/src/main/kotlin/cyou/shinobi9/cirrus/ui/CirrusUIApplication.kt
@@ -1,16 +1,27 @@
 package cyou.shinobi9.cirrus.ui
 
 import cyou.shinobi9.cirrus.ui.view.MainView
+import javafx.event.EventHandler
+import javafx.stage.Stage
+import javafx.stage.WindowEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.javafx.JavaFx
 import mu.KotlinLogging
 import tornadofx.*
 import kotlin.coroutines.CoroutineContext
+import kotlin.system.exitProcess
 
 class CirrusUIApplication : App(MainView::class, Styles::class), CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.JavaFx
+
+    override fun start(stage: Stage) {
+        stage.onCloseRequest = EventHandler {
+            exitProcess(0)
+        }
+        super.start(stage)
+    }
 }
 
 fun main(args: Array<String>) = launch<CirrusUIApplication>(args)
