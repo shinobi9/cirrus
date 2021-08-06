14251f434d04a2b5f2718feef58489e56ca47502
shinobi9 <shinobi9c@outlook.com>
2021-08-06 10:29:15 +0800

    add log config to let me find some message (type) which is not knew yet

---
+++ cirrus-ui/src/test/kotlin/main.kt
@@ -0,0 +1,7 @@
+import cyou.shinobi9.cirrus.ui.CirrusUIApplication
+import tornadofx.launch
+
+fun main(args: Array<String>) {
+    launch<CirrusUIApplication>(args)
+//    System.getProperties().list(System.out)
+}
